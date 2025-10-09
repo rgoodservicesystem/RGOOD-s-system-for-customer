176e930ac1c124f7da141f39868ef2967af961d8
rgoodservicesystem <rgoodserviceka@gmail.com>
2025-10-09 21:16:03 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+<script>
+  window.APP_CONFIG = {
+    SUPABASE_URL: "https://qmugpalgcktfwixqepti.supabase.co",
+    SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFtdWdwYWxnY2t0ZndpeHFlcHRpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg4NzI3NjcsImV4cCI6MjA3NDQ0ODc2N30.cIOCbJAr8_Gbg8d5IxpTQZKVRgl1ETN1hyBgL2_LveA",
+    WARNING_DAYS: 90,
+    REDIRECT_ADMIN: "admin.html",
+    REDIRECT_CLIENT: "customer.html"
+  };
+</script>
