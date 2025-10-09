a3b80e85632c0c260235ae8d64e4dc93f1852e39
rgoodservicesystem <rgoodserviceka@gmail.com>
2025-10-09 21:25:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 <script>
   window.APP_CONFIG = {
-    SUPABASE_URL: "https://qmugpalgcktfwixqepti.supabase.co",
-    SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFtdWdwYWxnY2t0ZndpeHFlcHRpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg4NzI3NjcsImV4cCI6MjA3NDQ0ODc2N30.cIOCbJAr8_Gbg8d5IxpTQZKVRgl1ETN1hyBgL2_LveA",
+    SUPABASE_URL: "https://phwigdhupzunbkywgxiq.supabase.co",
+    SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBod2lnZGh1cHp1bmJreXdneGlxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjAwMTk2NjgsImV4cCI6MjA3NTU5NTY2OH0.5mWxBvX_VTZIpUOJiOvNAj-Vh1XEH4SNvbpk3YGuOEA",
     WARNING_DAYS: 90,
     REDIRECT_ADMIN: "admin.html",
     REDIRECT_CLIENT: "customer.html"
