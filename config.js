626ca95f1d3dd581d90d4d94acb5fef05204150a
rgoodservicesystem <rgoodserviceka@gmail.com>
2025-10-11 13:09:45 +0700

    Delete config.js
    Signed-off-by: rgoodservicesystem <rgoodserviceka@gmail.com>

--- config.js
+++
@@ -1,9 +0,0 @@
-<script>
-  window.APP_CONFIG = {
-    SUPABASE_URL: "https://phwigdhupzunbkywgxiq.supabase.co",
-    SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBod2lnZGh1cHp1bmJreXdneGlxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjAwMTk2NjgsImV4cCI6MjA3NTU5NTY2OH0.5mWxBvX_VTZIpUOJiOvNAj-Vh1XEH4SNvbpk3YGuOEA",
-    WARNING_DAYS: 90,
-    REDIRECT_ADMIN: "admin.html",
-    REDIRECT_CLIENT: "customer.html"
-  };
-</script>
